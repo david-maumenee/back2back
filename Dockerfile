70eb7988d6d4f126ca76ba8790414ee7da4b36f9
David MAUMENEE <d.maumenee@groupeonepoint.com>
2017-01-29 17:35:20 +0100

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node4-onbuild
+# replace this with your application's default port
+EXPOSE 80
--- main.js
+++ main.js
@@ -2,60 +2,53 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.createServer(app);
-var port = process.env.PORT;
-var uuid = process.env.CNTM_INSTANCE_UUID;
+var backPort = process.env.BACK_PORT;
+var backHost = process.env.BACK_HOST;
 var app2link = process.env.APP2LINK_URI;
-var ipandport = "";
+var hostname = "";
+var backResponse = "";
+var response;
 
 app.get('/', function (req, res) {
-	res.end("see APCERA app logs");
+	callTheBack();
+	response = res;
+	
 });
 
 
-function callapp2() {
-	if (app2link) {
-		ipandport = getIpPort();
+function callTheBack() {
+			
 		var options = {
-			host: ipandport[0],
-			port: parseInt(ipandport[1]),
+			host: backHost,
+			port:  parseInt(backPort),
 			path: '/'
 		};
 
 		http.get(options, function (res) {
 			res.on("data", function (chunk) {
-				console.log(getLogPrefix() + "call " + chunk + " | " + ipandport[0]);
+				backResponse = chunk;
 			});
 		}).on('error', function (e) {
-			console.log(getLogPrefix() + "Got error: " + e.message);
+			backResponse = e.message;
 		});
-	} else {
-		console.log(getLogPrefix() + "JOB link not SET !");
-	}
+	response.end("back hostname" + hostname + " Back2Response " + backResponse)
 };
 
 function getLogPrefix(){
-	return new Date() + "| app1 " + uuid + " | ";
+	return new Date() + "| back2back " + hostname + " | ";
 }
 
-function getIpPort(){
-	var app2link = process.env.APP2LINK_URI;
-	return app2link.slice(6).split(':');
-}
+
 
 server.listen(port, function () {
+	hostname = server.address()
 	console.log('#########################################################################################################');
-	console.log(getLogPrefix() + "Start. app2link=" + app2link);
-	if (app2link) {
-		ipandport = getIpPort();
-
-		console.log(getLogPrefix() + 'app2 job link ip :' + ipandport[0] + ', port :' + ipandport[1]);
-
-		setInterval(callapp2, 2000);
-
-	} else {
-		console.log(getLogPrefix() + "JOB link not SET !");
+	console.log(getLogPrefix() + "Start." );
+	if (!backHost) {
+		backHost = "back";
+		backPort="80";
 	}
-
+	console.log(getLogPrefix() + " backHost=" + backHost + " backPort=" + backPort );
 
 
 });
